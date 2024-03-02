f269fce5365857f36cbb65c061a415eeb1a3c1e4
aiturann <aiturgannnnnn@gmail.com>
2024-03-02 14:42:09 +0600

    Initial Commit

---
+++ POP4lesson7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POP4lesson7
+//
+//  Created by Aiturgan Kurmanbekova on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
